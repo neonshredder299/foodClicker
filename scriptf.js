ce7863c994a0b6f15328bfc8a8d323195a6517c7
0wifi <104783815+0wifi@users.noreply.github.com>
2022-12-07 17:41:38 -0600

    Update scriptf.js

--- scriptf.js
+++ scriptf.js
@@ -10,54 +10,55 @@ document.getElementById("weight").innerHTML = weight;
 update();
 
 function update(){
-
-    document.getElementById("food").innerHTML = Math.round(food);
-    document.getElementById("per").innerHTML = Math.round(perClick);
-    document.getElementById("req").innerHTML = Math.round(req);
-    document.getElementById("add").innerHTML = Math.round(toAdd);
-    eric.width = width;
-    eric.height = height;
-    if(width<700)
-    {
-        document.getElementById("title").innerHTML = "Normal Eric (still fat)";
-        
-    }
-    else if(width<1500)
-    {
-        document.getElementById("title").innerHTML = "Fat Eric (fat)";
-        document.getElementById("eric").src = "IMG_9899.PNG";
-    }
-    else if(width<3750)
-    {
-        document.getElementById("title").innerHTML = "HUGE HUMONGOUS ERIC";
-        document.getElementById("eric").src = "pumpkin king.png";
-    }
-    else{
-        document.getElementById("title").innerHTML = "COLLOSAL GOBLIN ERIC";
-        document.getElementById("eric").src = "goblin.png";
-    }
-
+  document.getElementById("food").innerHTML = Math.round(food);
+  document.getElementById("per").innerHTML = Math.round(perClick);
+  document.getElementById("req").innerHTML = Math.round(req);
+  document.getElementById("add").innerHTML = Math.round(toAdd);
+  eric.width = width;
+  eric.height = height;
+  if(weight<5000)
+  {
+    document.getElementById("title").innerHTML = "Normal Eric (still fat)"; 
+  }
+  else if(weight<15000)
+  {
+    document.getElementById("title").innerHTML = "Fat Eric (fat)";
+    document.getElementById("titleDiv").style.background = "#e670ca";
+    document.getElementById("eric").src = "IMG_9899.PNG";
+  }
+  else if(weight<30000)
+  {
+    document.getElementById("title").innerHTML = "PUMPKIN KING ERIC";
+    document.getElementById("titleDiv").style.background = "#f54905";
+    document.getElementById("eric").src = "pumpkin king.png";
+  }
+  else{
+    document.getElementById("title").innerHTML = "COLLOSAL GOBLIN ERIC";
+    document.getElementById("titleDiv").style.background = "#178708";
+    document.getElementById("eric").src = "goblin.png";
+  }
 }
 function increment(){
-    food+=perClick;
-    update();
+  food+=perClick;
+  update();
 }
+
 function check()
 {
-    if(food>=req)
-    {
-        perClick+=toAdd;
-        food-=req;
-        width+=req/50;
-        height+=req/50;
-        req*=1.1;
-        toAdd+=0.2;
-        eat();
-        update();
-    }
+  if(food>=req)
+  {
+    perClick+=toAdd;
+    food-=req;
+    width+=10;
+    height+=10;
+    req*=1.1;
+    toAdd+=0.2;
+    eat();
+    update();
+  }
 }
 function eat()
 {
-    weight+=Math.floor(Math.random() * 1000) + 50;
-    document.getElementById("weight").innerHTML = weight;
+  weight+=Math.floor(Math.random() * 1000) + 50;
+  document.getElementById("weight").innerHTML = weight;
 }
